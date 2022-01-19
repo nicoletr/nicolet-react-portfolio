02e064b9904ca85c93937d9f454f51f666434783
Nicoletr <nicole.trpevski@gmail.com>
2022-01-19 22:20:06 +0800

    Imported styling to NavTabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,7 +1,6 @@
 import React from "react";
+import "../styles/App.css";
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
