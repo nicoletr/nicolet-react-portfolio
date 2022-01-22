4adda0c5b92fa4cb00ae0bf5f68e96f616d8b825
Nicoletr <nicole.trpevski@gmail.com>
2022-01-22 12:35:55 +0800

    Contact page created

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import "../../styles/App.css";
+import { validateEmail } from "../../utils/helpers";
+
+function ContactForm() {
+  // Create state variables for the fields in the form
+  // We are also setting their initial values to an empty string
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "name") {
+      setName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    if (!validateEmail(email) || !name) {
+      setErrorMessage(
+        "Please fill out both name and email - I'd love to know who you are and how to contact you!"
+      );
+      // We want to exit out of this code block if something is wrong so that the user can correct it
+      return;
+      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+    }
+    alert(`Thanks for contacting me ${name}! I will be in touch soon :)`);
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <p>Drop me a line</p>
+      <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Send!
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ContactForm;
